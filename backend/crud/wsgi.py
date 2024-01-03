d39b4a98aa0ff2cef10c50c650cbc97773409698
Mike Stromme <mstromme@suburbanenterprises.com>
2024-01-03 13:03:07 -0600

    x

--- backend/crud/wsgi.py
+++ backend/crud/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for crud project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crud.settings")
 
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crud.settings")
     try:
         from django.core.management import execute_from_command_line
