91b41ec82d9bf34243db2e2c2e8944f425f7bc66
Mike Stromme <mikestromme@gmail.com>
2024-02-03 07:45:20 -0600

    view update

--- backend/api/views.py
+++ backend/api/views.py
@@ -37,6 +37,19 @@ class ProjectManagerViewset(viewsets.ViewSet):
         queryset = ProjectManager.objects.all()
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(): 
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors, status=400)
+        
+    def destroy(self, request, pk=None):
+        ProjectManager = self.queryset.get(pk=pk)
+        ProjectManager.delete()
+        return Response(status=204)
 
 
 class ProjectViewset(viewsets.ViewSet):
